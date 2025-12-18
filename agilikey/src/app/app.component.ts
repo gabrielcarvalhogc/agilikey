be5f32e4fec34b09900c6a20a0eaa50661ff1e77
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-12-18 15:21:41 -0300

    feat: implement side menu component and restructure app layout

--- agilikey/src/app/app.component.ts
+++ agilikey/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { KeyboardFullComponent } from "./features/keyboard-full/keyboard-full.component";
+import { SideMenuComponent } from "./features/side-menu/side-menu.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, KeyboardFullComponent],
+  imports: [RouterOutlet, KeyboardFullComponent, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
